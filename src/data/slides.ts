e97364212074f7e552963187fbbcfd1a2370288a
QuickLink <register@red-it.org>
2025-07-06 15:52:24 +0200

    Change Index Change slide image url

--- src/data/slides.ts
+++ src/data/slides.ts
@@ -1,21 +1,21 @@
 export const slides = [
   {
-    image: 'https://red-it.org/apps/sandra/images/Kinderzimmer.webp',
+    image: 'https://www.pikosfusion.de//images/Kinderzimmer.webp',
     title: 'Kinderzimmer',
     subtitle: 'Zeit Träume zu leben - So individuell wie du bist, so einzigartig ist dein Raum.',
   },
   {
-    image: 'https://www.red-it.org/apps/sandra/images/Warteraum.webp',
+    image: 'https://www.pikosfusion.de//images/Warteraum.webp',
     title: 'Wartezimmer',
     subtitle: 'Wo Wartezeit zur Qualitätszeit wird - Entspannen Sie in einer einladenden Atmosphäre.',
   },
   {
-    image: 'https://red-it.org/apps/sandra/images/Therapieraum.webp',
+    image: 'https://www.pikosfusion.de//images/Therapieraum.webp',
     title: 'Therapieräume',
     subtitle: 'Entdecken, Wachsen, Entwickeln - Hier wird Lernen zur Freude.',
   },
   {
-    image: 'https://red-it.org/apps/sandra/images/Kita-Einrichtung.webp',
+    image: 'https://www.pikosfusion.de//images/Kita-Einrichtung.webp',
     title: 'Kita-Einrichtung',
     subtitle: 'Ein Raum voller Möglichkeiten – kindgerecht, funktional und mit Liebe zum Detail gestaltet.',
   },
